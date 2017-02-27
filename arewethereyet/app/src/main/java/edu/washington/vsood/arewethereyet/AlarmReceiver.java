a15fe83e88d065dfb9560613a32415116bcbd604
Vishesh Sood <omg.itsvishesh@gmail.com>
2017-02-26 17:47:10 -0800

    added texting feature/

--- arewethereyet/app/src/main/java/edu/washington/vsood/arewethereyet/AlarmReceiver.java
+++ arewethereyet/app/src/main/java/edu/washington/vsood/arewethereyet/AlarmReceiver.java
@@ -3,7 +3,8 @@ package edu.washington.vsood.arewethereyet;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
+import android.telephony.SmsManager;
+
 
 /**
  * Created by vishesh on 2/18/17.
@@ -13,8 +14,10 @@ public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        String message = intent.getStringExtra("number") + ": " + intent.getStringExtra("message");
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+//        String message = intent.getStringExtra("number") + ": " + intent.getStringExtra("message");
+//        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(intent.getStringExtra("number"), null, intent.getStringExtra("message"), null, null);
     }
 }
